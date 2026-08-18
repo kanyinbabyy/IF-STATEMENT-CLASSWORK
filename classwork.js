2add4c44b2b46d259ae8e1d95fc44c40af7e87f6
Kanyinsola Esther Oladeinde <keoladeinde@sidmach.com>
2026-08-18 11:52:53 +0100

    if classwork

---
+++ classwork.js
@@ -0,0 +1,22 @@
+let userName = "Admin"
+let password = "TheMaster";
+
+if (userName === "Admin")
+{
+    if (password === "TheMaster")
+    {
+        console.log("Welcome!");
+    }
+    else if( password === "")
+    {
+        console.log("Canceled");
+    }
+    else {
+        console.log ("Wrong Password");
+    }
+}
+else if (userName === "") {
+    console.log("Canceled")
+} else {
+    console.log ("I don't know you");
+}
